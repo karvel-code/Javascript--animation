75bd1e79eb4be451f6a5f7171d7c97b0fa7dde56
karvel-code <velgithinji@gmail.com>
2021-10-28 10:31:50 +0300

    Add animation for hero section

--- app.js
+++ app.js
@@ -6,4 +6,5 @@ const headline = document.querySelector(".headline");
 
 const tl = new TimelineMax();
 
-tl.fromTo(hero,1, {height: "0%"}, {height: "80%"})
+tl.fromTo(hero,1, {height: "0%"}, {height: "80%", ease: Power2.easeInOut})
+.fromTo(slider, 1.2, { x: "-100%" }, {x: "0%", ease: Power2.easeInOut });
