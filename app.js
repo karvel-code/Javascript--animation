87268424202533c01aa98f009ac59c2905b9ed3a
karvel-code <velgithinji@gmail.com>
2021-10-28 10:13:52 +0300

    Add app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+const hero = document.querySelector(".hero");
+const slider = document.querySelector(".slider");
+const logo = document.querySelector("#logo");
+const hamburger = document.querySelector(".hamburger");
+const headline = document.querySelector(".headline");
+
