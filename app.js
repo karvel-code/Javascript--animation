fcd98c60ae1bc0f080d55dbb30f299b7ebba34c3
karvel-code <velgithinji@gmail.com>
2021-10-28 10:23:49 +0300

    Add animation for image container

--- app.js
+++ app.js
@@ -4,3 +4,6 @@ const logo = document.querySelector("#logo");
 const hamburger = document.querySelector(".hamburger");
 const headline = document.querySelector(".headline");
 
+const tl = new TimelineMax();
+
+tl.fromTo(hero,1, {height: "0%"}, {height: "80%"})
